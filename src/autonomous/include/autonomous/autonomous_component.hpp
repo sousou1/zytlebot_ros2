847e773ae514d5893be140a33c99535256e4a619
sousou1 <tamusou6953@gmail.com>
2019-05-31 13:09:03 +0900

    no message

--- src/autonomous/include/autonomous/autonomous_component.hpp
+++ src/autonomous/include/autonomous/autonomous_component.hpp
@@ -104,6 +104,8 @@ namespace autonomous
         rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_pub;
         rclcpp::Publisher<std_msgs::msg::String>::SharedPtr signal_search_;
 
+        void red_flag_update();
+        void image_cb();
         // void autonomous();
     };
 
